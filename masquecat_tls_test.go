c7cd934b6730e91b45193995f09346544d54e2bf
Albert Hsieh <knowlet@users.noreply.github.com>
2026-09-03 09:02:05 +0800

    test(masque): cover dynamic TLS certificate sources

--- masquecat_tls_test.go
+++ masquecat_tls_test.go
@@ -3,6 +3,7 @@
 package tailcat
 
 import (
+	"crypto/tls"
 	"testing"
 
 	"github.com/quic-go/quic-go/http3"
@@ -34,5 +35,26 @@ func TestMasqueTLSClientConfigInsecureOptIn(t *testing.T) {
 	}
 	if cfg.ServerName != "127.0.0.1" {
 		t.Fatalf("ServerName = %q", cfg.ServerName)
+	}
+}
+
+func TestHasMasqueServerCertificate(t *testing.T) {
+	tests := []struct {
+		name string
+		conf *tls.Config
+		want bool
+	}{
+		{name: "nil", conf: nil, want: false},
+		{name: "empty", conf: &tls.Config{}, want: false},
+		{name: "certificate", conf: &tls.Config{Certificates: []tls.Certificate{{}}}, want: true},
+		{name: "get-certificate", conf: &tls.Config{GetCertificate: func(*tls.ClientHelloInfo) (*tls.Certificate, error) { return nil, nil }}, want: true},
+		{name: "get-config-for-client", conf: &tls.Config{GetConfigForClient: func(*tls.ClientHelloInfo) (*tls.Config, error) { return nil, nil }}, want: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hasMasqueServerCertificate(tt.conf); got != tt.want {
+				t.Fatalf("hasMasqueServerCertificate() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
